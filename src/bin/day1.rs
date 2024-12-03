7bb378c6bbb462f66fcda3f69366d77e9217aba4
Lucas Bombarda (Note-UL) <lucas@devdock.com.br>
2024-12-03 15:38:18 -0300

    day1 part 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,34 +1,31 @@
 mod utils;
 
-use std::{collections::HashMap, usize};
 use utils::file::read_content;
 
-fn part_one(content: &str) -> i32 {
-    let mut map_first_col: HashMap<usize, usize> = HashMap::new();
-    let mut map_second_col: HashMap<usize, usize> = HashMap::new();
+fn part_one(content: &str) -> usize {
+    let mut first_col: Vec<usize> = Vec::new();
+    let mut second_col: Vec<usize> = Vec::new();
+    let mut result = 0;
 
     for line in content.lines() {
-        if let Some((num_first_col, num_second_col)) = line.split_once("   ") {
-            let first_num = num_first_col.parse::<usize>().unwrap();
-            let sec_num = num_second_col.parse::<usize>().unwrap();
-
-            map_first_col
-                .entry(first_num)
-                .and_modify(|x| *x += 1)
-                .or_insert(1);
-
-            map_second_col
-                .entry(sec_num)
-                .and_modify(|x| *x += 1)
-                .or_insert(1);
-
-            println!("{num_first_col} - {num_second_col}");
+        if let Some((first, second)) = line.split_once("   ") {
+            first_col.push(first.parse().unwrap());
+            second_col.push(second.parse().unwrap());
         }
+
+        first_col.sort();
+        second_col.sort();
     }
 
-    println!("{:?}", map_first_col);
-    println!("{:?}", map_second_col);
-    1
+    for i in 0..first_col.len() {
+        let diff = if first_col[i] > second_col[i] {
+            first_col[i] - second_col[i]
+        } else {
+            second_col[i] - first_col[i]
+        };
+        result += diff;
+    }
+    result
 }
 
 fn main() {
