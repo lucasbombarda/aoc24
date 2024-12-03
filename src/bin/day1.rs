56cf9e9cc809bad58dc2ead03b48aa7635d22df4
Lucas Bombarda (Note-UL) <lucas@devdock.com.br>
2024-12-03 15:56:03 -0300

    day1 part 2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,5 +1,8 @@
 mod utils;
 
+use core::str;
+use std::collections::HashMap;
+
 use utils::file::read_content;
 
 fn part_one(content: &str) -> usize {
@@ -28,8 +31,35 @@ fn part_one(content: &str) -> usize {
     result
 }
 
+fn part_two(content: &str) -> usize {
+    let mut first_col: Vec<usize> = Vec::new();
+    let mut second_col: HashMap<usize, usize> = HashMap::new();
+
+    for line in content.lines() {
+        if let Some((first, second)) = line.split_once("   ") {
+            first_col.push(first.parse().unwrap());
+            let second = second.parse().unwrap();
+            second_col
+                .entry(second)
+                .and_modify(|e| *e += 1)
+                .or_insert(1);
+        }
+    }
+
+    let mut result = 0;
+    for i in first_col.iter() {
+        if let Some(number) = second_col.get(&i) {
+            result += i * number;
+        }
+    }
+    result
+}
+
 fn main() {
     let content = read_content("./src/input/day1.txt");
     let first = part_one(&content);
-    println!("{first}");
+    println!("Part one: {first}");
+
+    let second = part_two(&content);
+    println!("Part two: {second}");
 }
