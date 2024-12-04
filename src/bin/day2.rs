b1ea8ac0bc326b9c0ee698e84d61efa1f7e8c9d5
Lucas Bombarda <lucas@devdock.com.br>
2024-12-03 21:15:22 -0300

    day 2 part 1

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -2,8 +2,59 @@ mod utils;
 
 use utils::file::read_content;
 
+#[derive(Debug, PartialEq)]
+enum Direction {
+    Ascending,
+    Descending,
+}
+
 fn part_one(content: &str) -> usize {
-    0
+    let mut result = 0;
+    for line in content.lines() {
+        let records = line
+            .split_whitespace()
+            .map(|x| (*x).parse::<isize>().unwrap())
+            .collect::<Vec<isize>>();
+
+        let mut direction: Option<Direction> = None;
+        let mut is_valid = true;
+
+        for i in 0..records.len() - 1 {
+            let diff = records[i + 1] - records[i];
+
+            if diff.abs() > 3 {
+                is_valid = false;
+                break;
+            }
+
+            if let Some(dir) = &direction {
+                match dir {
+                    Direction::Ascending if diff <= 0 => {
+                        is_valid = false;
+                        break;
+                    }
+                    Direction::Descending if diff >= 0 => {
+                        is_valid = false;
+                        break;
+                    }
+                    _ => {}
+                }
+            } else {
+                direction = if diff > 0 {
+                    Some(Direction::Ascending)
+                } else if diff < 0 {
+                    Some(Direction::Descending)
+                } else {
+                    is_valid = false;
+                    break;
+                }
+            }
+        }
+        if is_valid {
+            result += 1;
+        }
+    }
+    result
 }
 
 fn part_two(content: &str) -> usize {
