6027a6f883db5468a53944e2de74699dc65d2eb8
Lucas Bombarda <lucas@devdock.com.br>
2024-12-04 21:25:45 -0300

    day 3 test str

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -7,9 +7,9 @@ fn part_one(content: &str) -> i32 {
     let pattern = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
     let mut result = 0;
     content.lines().into_iter().for_each(|x| {
-        for m in pattern.captures_iter(x) {
-            result += &m[1].parse::<i32>().unwrap() * &m[2].parse::<i32>().unwrap()
-        }
+        pattern
+            .captures_iter(x)
+            .for_each(|m| result += &m[1].parse::<i32>().unwrap() * &m[2].parse::<i32>().unwrap());
     });
     result
 }
@@ -31,16 +31,19 @@ fn main() {
 mod tests {
     use super::*;
 
-    const TEST_STR: &str =
+    const TEST_STR_1: &str =
         "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+
+    const TEST_STR_2: &str =
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     #[test]
     fn test_part_one() {
-        assert_eq!(part_one(TEST_STR), 161)
+        assert_eq!(part_one(TEST_STR_1), 161)
     }
 
     #[test]
     fn test_part_two() {
-        assert_eq!(part_two(TEST_STR), 0)
+        assert_eq!(part_two(TEST_STR_2), 48)
     }
 }
